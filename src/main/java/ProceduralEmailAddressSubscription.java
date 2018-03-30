da995043fd7aa38cc9a6c97d475a8926727b5899
ozihler <oliver.zihler@zuehlke.com>
2018-03-30 13:24:47 +0200

    Add tests, change mockito version.

--- src/main/java/ProceduralEmailAddressSubscription.java
+++ src/main/java/ProceduralEmailAddressSubscription.java
@@ -1,6 +1,9 @@
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
@@ -9,38 +12,34 @@ public class ProceduralEmailAddressSubscription {
     private static final Pattern EMAIL_ADDRESS_VALID_FORM = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
     public static void main(String[] args) throws IOException {
-        addSubscribers(System.in, System.out);
+        addSubscribers(System.out, new BufferedReader(new InputStreamReader(System.in)));
     }
 
-    static void addSubscribers(InputStream in, PrintStream out) {
+    static void addSubscribers(PrintStream out, BufferedReader bufferedReader) throws IOException {
         List<String> subscribers = new ArrayList<>();
 
         while (true) {
-            try {
-                //get an email address from the input field
-                out.println("Enter email address for new subscriber: ");
-                String emailAdress = new BufferedReader(new InputStreamReader(in)).readLine();
+            //get an email address from the input field
+            out.println("Enter email address for new subscriber: ");
+            String emailAdress = bufferedReader.readLine();
 
-                //validate the input
-                if (!(!StringUtils.isBlank(emailAdress) && EMAIL_ADDRESS_VALID_FORM.matcher(emailAdress).matches())) {
-                    throw new RuntimeException("Invalid Emailaddress");
-                }
-
-                //Store subscribers in database
-                subscribers.add(emailAdress);
-
-                //Show subscribers in overview
-                out.println("All subscribers: ");
-                out.println("================================");
-                for (String subscriber : subscribers) {
-                    out.println(subscriber);
-                }
-                out.println("================================");
-
-            } catch (Exception e) {
-                out.println(e.getMessage());
-                System.exit(-1);
+            //validate the input
+            if (!(!StringUtils.isBlank(emailAdress) && EMAIL_ADDRESS_VALID_FORM.matcher(emailAdress).matches())) {
+                out.println("Not an email address: "+emailAdress);
+                out.println("Program terminated");
+                return;
             }
+
+            //Store subscribers in database
+            subscribers.add(emailAdress);
+
+            //Show subscribers in overview
+            String output = "All subscribers:\n================================\n";
+            for (String subscriber : subscribers) {
+                output += subscriber + "\n";
+            }
+            output += "================================";
+            out.println(output);
         }
     }
 
--- src/test/java/ProceduralEmailAddressSubscriptionTest.java
+++ src/test/java/ProceduralEmailAddressSubscriptionTest.java
@@ -1,15 +1,46 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 
-import java.io.InputStream;
-import java.io.PrintStream;
+import java.io.*;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 public class ProceduralEmailAddressSubscriptionTest {
+
+    public static final String VALID_EMAIL_ADDRESS = "oliver.zihler@hotmail.com";
+    public static final String STOP_COMMAND = "stop";
+    public static final String VALID_EMAIL_ADDRESS_2 = "o.a@h.c";
+
+
     @Test
     public void main() throws Exception {
-        ProceduralEmailAddressSubscription.addSubscribers(mock(InputStream.class), mock(PrintStream.class));
+
+
+        BufferedReader inputMock = mock(BufferedReader.class);
+        when(inputMock.readLine()).thenReturn(VALID_EMAIL_ADDRESS)
+                .thenReturn(VALID_EMAIL_ADDRESS_2)
+                .thenReturn(STOP_COMMAND);
+
+        PrintStream out = mock(PrintStream.class);
+        ProceduralEmailAddressSubscription.addSubscribers(out, inputMock);
+
+        verify(out, times(3)).println("Enter email address for new subscriber: ");
+        verify(out, times(1)).println("All subscribers:\n" +
+                "================================\n" +
+                VALID_EMAIL_ADDRESS+"\n" +
+                "================================");
+        verify(out, times(1)).println("All subscribers:\n" +
+                "================================\n" +
+                VALID_EMAIL_ADDRESS+"\n" +
+                VALID_EMAIL_ADDRESS_2+"\n" +
+                "================================");
+        verify(out, times(1)).println("Not an email address: "+STOP_COMMAND);
+        verify(out, times(1)).println("Program terminated");
+
     }
 
 }
