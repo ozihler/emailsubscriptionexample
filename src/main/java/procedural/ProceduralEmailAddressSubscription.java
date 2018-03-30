8e67a5462f3cecf2886eb66c677f1a38b66a9097
ozihler <oliver.zihler@zuehlke.com>
2018-03-30 14:56:18 +0200

    Change structure of main class.
    (cherry picked from commit af79a6f)
    
    (cherry picked from commit 0af8d88)

--- src/main/java/procedural/ProceduralEmailAddressSubscription.java
+++ src/main/java/procedural/ProceduralEmailAddressSubscription.java
@@ -13,18 +13,26 @@ import java.util.regex.Pattern;
 public class ProceduralEmailAddressSubscription {
     private static final Pattern EMAIL_ADDRESS_VALID_FORM = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
 
-    public static void main(String[] args) throws IOException {
-        new ProceduralEmailAddressSubscription().addSubscribers(System.out, new BufferedReader(new InputStreamReader(System.in)));
+    private final PrintStream out;
+    private final BufferedReader in;
+
+    public ProceduralEmailAddressSubscription(PrintStream out, BufferedReader in) {
+        this.out = out;
+        this.in = in;
     }
 
-    public void addSubscribers(PrintStream out, BufferedReader bufferedReader) throws IOException {
+    public static void main(String[] args) throws IOException {
+        new ProceduralEmailAddressSubscription(System.out, new BufferedReader(new InputStreamReader(System.in))).addSubscribers();
+    }
+
+    public void addSubscribers() throws IOException {
         List<String> subscribers = new ArrayList<>();
 
         while (true) {
             //get an email address from the input field
             out.println("Enter email address for new subscriber: ");
 
-            String emailAddress = bufferedReader.readLine();
+            String emailAddress = in.readLine();
 
             //validate the input
             if (!(!StringUtils.isBlank(emailAddress) && EMAIL_ADDRESS_VALID_FORM.matcher(emailAddress).matches())) {
